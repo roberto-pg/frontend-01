02431ac9ef3b01ca80ee4bedf471ecac27a76e38
Roberto <roberto.p.gomes@hotmail.com>
2022-06-29 11:18:42 -0300

    Third class

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,4 +1,5 @@
 import { CheckCircle, Lock } from 'phosphor-react'
+import { Link } from 'react-router-dom'
 import { formatDate } from '../utils/format-date'
 
 interface LessonProps {
@@ -13,10 +14,10 @@ export function Lesson(props: LessonProps) {
   const availableDateFormatted = formatDate(props.availableAt)
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${props.slug}`} className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
@@ -36,6 +37,6 @@ export function Lesson(props: LessonProps) {
 
         <strong className="text-gray-200 mt-5 block">{props.title}</strong>
       </div>
-    </a>
+    </Link>
   )
 }
