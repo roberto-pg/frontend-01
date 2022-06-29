b3412b5cc717be50caa0da32340affbb823e9497
Roberto <roberto.p.gomes@hotmail.com>
2022-06-29 11:18:42 -0300

    Second class

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4sqkanb2e3801zcd5ecai6u/master",
-  cache: new InMemoryCache(),
-});
+  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4sqkanb2e3801zcd5ecai6u/master',
+  cache: new InMemoryCache()
+})
